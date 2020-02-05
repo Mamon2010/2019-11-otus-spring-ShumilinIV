52d14b899c4ab30b23109f0510a3ec04fb431c5b
Ivan Shumilin <ishumilin@list.ru>
2020-02-06 00:56:02 +0300

    Revert "Переделал с использованием Reduce"
    This reverts commit 003021a4a31ca249b8382c996b741e21c5de2397.

--- lesson2/homework2.js
+++ lesson2/homework2.js
@@ -1,27 +1,30 @@
 var fn1 = () => {
-    console.log('fn1')
-    return Promise.resolve(1);
-}
+	console.log('fn1');
+	return Promise.resolve(1);
+};
 
-var fn2 = () => new Promise(resolve => {
-    console.log('fn2')
-    setTimeout(() => resolve(2), 1000);
-})
+var fn2 = () =>
+	new Promise((resolve) => {
+		console.log('fn2');
+		setTimeout(() => resolve(2), 1000);
+	});
 
 function promiseReduce(asyncFunctions, reduce, initialValue) {
-    return asyncFunctions.reduce((prev, elem) =>
-        prev
-        .then(elem)
-        .then((val) => reduce(initialValue, val)), Promise.resolve());
+	return new Promise((resolve, reject) => {
+		let promise = asyncFunctions[0]();
+		promise
+			.then((value) => reduce(initialValue, value))
+			.then((value) => asyncFunctions[1]())
+			.then((value) => reduce(initialValue, value))
+			.then((value) => resolve(value));
+	});
 }
 
-
 promiseReduce(
-        [fn1, fn2],
-        function(memo, value) {
-            console.log('reduce')
-            return memo * value
-        },
-        1
-    )
-    .then(console.log);
+	[ fn1, fn2 ],
+	function(memo, value) {
+		console.log('reduce');
+		return memo * value;
+	},
+	1
+).then(console.log);
